43df7eafa848261a43788ac3f8e6799ab01f264e
UndermanCrak <victor46hug@gmail.com>
2024-08-17 09:12:00 -0600

    Primer proyecto

---
+++ public/imagenes.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Controlar el intervalo de cambio de las imágenes del carrusel
+    console.log("entrando ala funcion para el carrusel de imagenes");
+    $('.carousel').carousel({
+        interval: 2000 // Cambiar cada 2 segundos
+    });
+});
+
+console.log("Información general");
+console.warn("Advertencia");
+console.error("Error");
+
